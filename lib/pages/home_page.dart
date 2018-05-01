03f8652ed701f45bd16d7946af86d9034e541bdc
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 07:59:39 +0200

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+ 
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.blue,
+      child: new InkWell(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("Hello Flutter", style: new TextStyle(color: Colors.white, fontSize: 30.0)),
+              new Text("what else...", style: new TextStyle(color: Colors.black, fontSize: 15.0))
+            ],
+          ),
+      ),
+    );
+ }
+}
